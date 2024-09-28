db7ba8ba5c7a6c6ab995c4b5e8b850ee41154b67
20211576 <k.gheytuli@student.tue.nl>
2024-09-29 00:29:08 +0200

    Function find_disjoint_cycles implemented

--- aux_functions/disjoint_cycles.py
+++ aux_functions/disjoint_cycles.py
@@ -9,4 +9,29 @@ def find_disjoint_cycles(graph: dict[str, dict[str, int]], n: int) -> list[list[
     :param n: length of cycles
     :return: list of disjoint cycles
     """
+    # Set to track nodes used in disjoint cycles
+    used_nodes = set()
+    disjoint_cycles = []
 
+    def dfs(node, start, depth, visited, path):
+        if depth == n:
+            if start in graph[node]:
+                # Check if the cycles is disjoint
+                if all(p not in used_nodes for p in path):
+                    disjoint_cycles.append(path + [start])
+                    used_nodes.update(path)  # Mark these nodes as used
+
+            return
+
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited and neighbor not in used_nodes:
+                dfs(neighbor, start, depth + 1, visited, path + [neighbor])
+
+        visited.remove(node)
+
+    for node in graph:
+        dfs(node, node, 1, set(), [node])
+
+    return disjoint_cycles
