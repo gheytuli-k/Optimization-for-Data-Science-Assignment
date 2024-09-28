f41609dd28d95c06780347aa7c1ba4d748da7cfc
20211576 <k.gheytuli@student.tue.nl>
2024-09-29 00:27:36 +0200

    Function skel find_disjoint_cycle added

--- aux_functions/disjoint_cycles.py
+++ aux_functions/disjoint_cycles.py
@@ -1,2 +1,12 @@
 def find_disjoint_cycles(graph: dict[str, dict[str, int]], n: int) -> list[list[str]]:
+    """
+    Find all the disjoint cycles of length n in the graph
+    :param graph: adjacency list representation of graph
+        - key: node
+        - value: Dictionary of neighbors and the weight of the edge
+            - key: neighbor
+            - value: weight
+    :param n: length of cycles
+    :return: list of disjoint cycles
+    """
 
